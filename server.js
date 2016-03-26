d70ce6a03b34561ef7ccb65944216ec8e3e8caaa
Dustin Wurtz <dtwurtz@gmail.com>
2016-03-25 23:02:41 -0700

    Finished step four. App can GET, POST, PUT, and DELETE

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var http = require('http');
 
 var fs = require('fs');
@@ -5,11 +7,36 @@ var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
 
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+var petsData;
+
+fs.readFile(petsPath, 'utf8', function(err, data) {
+  petsData = JSON.parse(data);
+});
+
+app.disable('x-powered-by');
+app.set('port', process.env.PORT || 5000);
+
+var morgan = require('morgan');
+app.use(morgan('short'));
+
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+app.get('/pets', function(req, res) {
+  res.send(petsData);
+})
+
+app.get('/pets/:index', function(req, res) {
+  var index = Number.parseInt(req.params.index);
+
+  if(Number.isNaN(index) || index < 0 || index >= petsData.length) {
+    return res.sendStatus(404);
+  }
+
+  res.send(petsData[index]);
+})
 
 app.post('/pets', function(req, res) {
   var age = parseInt(req.body.age);
@@ -17,62 +44,78 @@ app.post('/pets', function(req, res) {
   var name = req.body.name;
 
   if (!age || !kind || !name) {
-    res.writeHead(400, {"Content-Type": "text/plain"});
-    res.end("Bad Request");
+    return res.sendStatus(400);
   }
   else if(isNaN === age){
-    res.writeHead(400, {"Content-Type": "text/plain"});
-    res.end("Bad Request");
+    return res.sendStatus(400);
   }
   else {
-    fs.readFile(petsPath, 'utf8', function(err, data) {
-      if(err) {
-        throw err;
+    petsData.push({age, kind, name});
+
+    var petsJSON = JSON.stringify(petsData);
+
+    fs.writeFile(petsPath, petsJSON, function(writeErr) {
+      if (writeErr) {
+        throw writeErr;
       }
-      var pets = JSON.parse(data);
-      pets.push({age: age, kind: kind, name: name});
-
-      var petsJSON = JSON.stringify(pets);
-
-      fs.writeFile(petsPath, petsJSON, function(writeErr) {
-        if (writeErr) {
-          throw writeErr;
-        }
-      })
-      res.send(petsJSON);
     })
+
+    res.send({age, kind, name});
   }
 })
 
-app.get('/pets', function(req, res) {
-  res.writeHead(200, {"Content-Type": "application/json"});
-  fs.readFile(petsPath, 'utf8', function(err, data) {
-    var pets = JSON.stringify(data);
-    var petsParsed = JSON.parse(pets);
-    res.end(petsParsed);
+app.put('/pets/:index', function(req, res) {
+  var index = Number.parseInt(req.params.index);
+
+  if(Number.isNaN(index) || index < 0 || index >= petsData.length) {
+    return res.sendStatus(404);
+  }
+
+  var age = parseInt(req.body.age);
+  var kind = req.body.kind;
+  var name = req.body.name;
+
+  if (!age || !kind || !name) {
+    return res.sendStatus(400);
+  }
+
+  petsData[index] = {age, kind, name};
+
+  var petsJSON = JSON.stringify(petsData);
+
+  fs.writeFile(petsPath, petsJSON, function(writeErr) {
+    if (writeErr) {
+      throw writeErr;
+    }
   })
+
+  res.send({age, kind, name});
 })
 
-app.get('/pets/:index', function(req, res) {
-  var index = req.params.index;
-  if(index <= 1 && index >= 0) {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    fs.readFile(petsPath, 'utf8', function(err, data) {
-      var pets = JSON.parse(data)[index];
-      var petsParsed = JSON.stringify(pets);
-      res.end(petsParsed);
-    })
+app.delete('/pets/:index', function(req, res) {
+  var index = Number.parseInt(req.params.index);
+
+  if(Number.isNaN(index) || index < 0 || index >= petsData.length) {
+    return res.sendStatus(404);
   }
-  else {
-    res.writeHead(404, {"Content-Type": "text/plain"});
-    res.end("404: Page Not Found");
-  }
+
+  var petDelete = petsData.splice(index, 1);
+
+  var petsJSON = JSON.stringify(petsData);
+
+  fs.writeFile(petsPath, petsJSON, function(writeErr) {
+    if (writeErr) {
+      throw writeErr;
+    }
+  })
+  res.send(petDelete);
 })
+
 
 app.get('/*', function(req, res) {
   res.status(404).send('404: Page Not Found');
 })
 
-app.listen(5000, function() {
-  console.log('Starting a server on localhost:5000');
-})
+app.listen(app.get('port'), function() {
+  console.log('Listening on', app.get('port'));
+});
